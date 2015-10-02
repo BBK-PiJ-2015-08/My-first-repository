40b8e352057ae4b17347b6cf950ae71ce49db302
Jade <jadedickinson@gmail.com>
2015-10-02 22:56:49 +0100

    Removed cardNumber incrementer so it always happens up until it is equal to 5.

--- day1ex11.groovy
+++ day1ex11.groovy
@@ -1,5 +1,5 @@
+String h1, h2, h3, h4, h5
 String c1, c2, c3, c4, c5
-String hand1, hand2, hand3, hand4, hand5
 int cardNumber = 0
 boolean finished = false
 //while (!finished)
@@ -7,21 +7,19 @@ while (!finished) {
 	println "Enter card rank (1-10, J, Q or K) and suit (spades, hearts, diamonds, clubs) in format '1hearts'."
 	if (cardNumber != 5) {
 		if (card == 0) {
-			cardNumber = cardNumber +1
+			h1 = System.console().readLine()
 		} else if (card == 1) {
-			cardNumber = cardNumber +1
 		} else if (card == 2) {
-			cardNumber = cardNumber +1
 		} else if (card == 3) {
-			cardNumber = cardNumber +1
 		} else if (card == 4) {
-			cardNumber = cardNumber +1
 		}
+		cardNumber = cardNumber +1
 	} else {
 		finished = true
 	}
 //	System.console().readLine()
 }
+if (h1.substring(0,1)<=h2.substring(0,1))
 
 /** --------------------
 want to store a set of numbers

f533db36869a437b290432c76225d6a558970ce3
Jade <jadedickinson@gmail.com>
2015-10-02 23:47:00 +0100

    Added code to test if all cards are of same suit

--- day1ex11.groovy
+++ day1ex11.groovy
@@ -1,9 +1,9 @@
 String storage
 String rank1, rank2, rank3, rank4, rank5
 String suit1, suit2, suit3, suit4, suit5
+String pokerHand = "Nothing"
 int cardNumber = 0
 boolean finished = false
-//while (!finished)
 while (!finished) {
 	if (cardNumber != 5) {
 		print "Enter rank (1-10, J, Q or K): "
@@ -82,27 +82,22 @@ while (!finished) {
 	} else {
 		finished = true
 	}
-//	System.console().readLine()
 }
+if (suit1 == suit2 && suit1 == suit3 && suit1 == suit4 && suit1 == suit5) {
+	println "All the same"
+}
+
 //if (h1.substring(0,1)<=h2.substring(0,1))
 
-/** --------------------
-want to store a set of numbers
-then write these to consecutive cards
-
---------------------
-Straight flush: 1-5,2-6,3-7,4-8,5-9,6-10,7-J,8-Q,9-K & all same suit
-Flush: Any numbers & all same suit
-
-Straight: 1-5,2-6,3-7,4-8,5-9,6-10,7-J,8-Q,9-K & any suit
-
-Full house: 3 numbers are the same, 2 numbers the same
-Two pairs: 2 numbers are same & 2 other numbers the same
-
+/** ---------------
+>> Straight flush: 1-5,2-6,3-7,4-8,5-9,6-10,7-J,8-Q,9-K & all same suit
 Poker: 4 numbers are the same & any suit
+>> Flush: Any numbers & all same suit
+Full house: 3 numbers are the same, 2 numbers the same
+Straight: 1-5,2-6,3-7,4-8,5-9,6-10,7-J,8-Q,9-K & any suit
 Three of a kind: 3 numbers are the same
+Two pairs: 2 numbers are same & 2 other numbers the same
 Pair: 2 numbers are the same
-
 Nothing: any other situation
 
 if(s.substring)

cf53f62366162b7436676e1e8051fca3c9b321aa
Jade <jadedickinson@gmail.com>
2015-10-03 21:12:23 +0100

    Completed first go at program

--- day1ex16.groovy
+++ day1ex16.groovy
@@ -4,5 +4,19 @@ println "To enter 2 players' rock paper scissors moves, use the options PP, PR, 
 while (p1score-4 != p2score && p2score-4 !=) {
 	println "Enter an option: "
 	input = System.console().readLine()
-	if 
+	if (input = "PR") {
+		p1score = p1score+1
+	} else if (input = "PS") {
+		p2score = p2score+1
+	} else if (input = "RP") {
+		p2score = p2score + 1
+	} else if (input = "RS") {
+		p1score = p1score + 1
+	} else if (input = "SP") {
+		p1score = p1score + 1
+	} else if (input = "SR") {
+		p2score = p2score + 1
+	}
 }
+println "Player 1's score is " + p1score
+println "Player 2's score is " + p2score

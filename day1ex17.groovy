87224aee020f5c84e172e0e89f0bcc5bb040c9f7
Jade <jadedickinson@gmail.com>
2015-10-03 22:18:11 +0100

    Completed code to test if input is prime

--- day1ex17.groovy
+++ day1ex17.groovy
@@ -1,10 +1,19 @@
 print "Enter a number: "
+boolean result = true
 String str = System.console().readLine()
 int userInput = Integer.parseInt(str)
 int dividedBy = userInput - 1
 while (dividedBy>1) {
-	if ()
+	if (userInput&dividedBy == 0) {
+		result = false
+	}
+	dividedBy--
 }
+if (result) {
+	println "Your number, " + i + ", is a prime number."
+}
+else 
+if (result == false)
 if (++)
 
 if ((upper - userInput) = (lower - userInput)) {

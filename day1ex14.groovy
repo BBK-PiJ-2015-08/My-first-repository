d404834dca4730273dce6329a3019934d7c64f33
Jade <jadedickinson@gmail.com>
2015-10-03 12:47:27 +0100

    Modified program from ex 13 to give the first 1000 primes

---
+++ day1ex14.groovy
@@ -0,0 +1,19 @@
+boolean finished = false
+int nthPrime = 0
+for (i=2; i>-1; i++) {
+	if (nthPrime == 1000) {
+		break;
+	}
+	boolean result = true
+	int j = i-1
+	while (j>1) {
+		if (i%j == 0) {
+			result = false
+		}
+		j--
+	}
+	if (result == true) {
+	println i + " is a prime number."
+	nthPrime = nthPrime + 1
+	}
+}

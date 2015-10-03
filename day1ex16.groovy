47f381f1f5c1c118bb32810c06da309cf452ea61
Jade <jadedickinson@gmail.com>
2015-10-03 21:03:04 +0100

    Wrote start of program, before writing loop

---
+++ day1ex16.groovy
@@ -0,0 +1,4 @@
+String input
+int p1score, p2score = 0
+println "Enter 2 players' rock paper scissors moves, from the options PP, PR, PS, RP, RR, RS, SP, SR or SS."
+input = System.console().readLine()

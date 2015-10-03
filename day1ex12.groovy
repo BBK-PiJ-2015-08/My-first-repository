02dc4f78212ad1a5b94dbbf3aeb3e024365aeb52
Jade <jadedickinson@gmail.com>
2015-10-03 10:57:17 +0100

    Pyramid exercise and fixed primes exercise (reset result to true at start of for loop)

---
+++ day1ex12.groovy
@@ -0,0 +1,8 @@
+for (i=1; i<8; i++) {
+	int j = i
+	while (j>0) {
+		print i
+		j = j-1
+	}
+	println ""
+}
---
+++ day1ex13.groovy
@@ -0,0 +1,15 @@
+for (i=2; i<10; i++) {
+	boolean result = true
+	int j = i-1
+	while (j>1) {
+		if (i%j == 0) {
+			result = false
+		}
+		j--
+	}
+	if (result == true) {
+	println i + " is a prime number."
+	} else {
+		println i + " is not a prime number."
+	}
+}

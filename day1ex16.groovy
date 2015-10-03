7128219409120781b83057d320a4b99656e41c45
Jade <jadedickinson@gmail.com>
2015-10-03 21:16:37 +0100

    Corrected = signs to == signs

--- day1ex16.groovy
+++ day1ex16.groovy
@@ -1,20 +1,20 @@
 String input = ""
 int p1score, p2score = 0
 println "To enter 2 players' rock paper scissors moves, use the options PP, PR, PS, RP, RR, RS, SP, SR or SS."
-while (p1score-4 != p2score && p2score-4 !=) {
+while (p1score-4 != p2score && p2score-4 != p1score) {
 	println "Enter an option: "
 	input = System.console().readLine()
-	if (input = "PR") {
+	if (input == "PR") {
 		p1score = p1score+1
-	} else if (input = "PS") {
+	} else if (input == "PS") {
 		p2score = p2score+1
-	} else if (input = "RP") {
+	} else if (input == "RP") {
 		p2score = p2score + 1
-	} else if (input = "RS") {
+	} else if (input == "RS") {
 		p1score = p1score + 1
-	} else if (input = "SP") {
+	} else if (input == "SP") {
 		p1score = p1score + 1
-	} else if (input = "SR") {
+	} else if (input == "SR") {
 		p2score = p2score + 1
 	}
 }

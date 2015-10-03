5462c3eae1d2118a820ba7c6e88d65d601fefaab
Jade <jadedickinson@gmail.com>
2015-10-03 13:19:01 +0100

    Uploaded my program to let the user guess a number

---
+++ day1ex15.groovy
@@ -0,0 +1,19 @@
+int numGuesses = 1
+int yourGuess
+int numberToGuess = Math.abs(1000 * Math.random())
+println "Try to guess my number!"
+print "Tell me a number: "
+String storage = System.console().readLine()
+yourGuess = Integer.parseInt(storage)
+while (yourGuess != numberToGuess) {
+	if (numberToGuess > yourGuess) {
+		println "No! My number is higher."
+	} else {
+		println "No! My number is lower."
+	}
+	print "Tell me a number: "
+	storage = System.console().readLine()
+	yourGuess = Integer.parseInt(storage)
+	numGuesses = numGuesses + 1
+}
+println "You needed " + numGuesses + " guesses."

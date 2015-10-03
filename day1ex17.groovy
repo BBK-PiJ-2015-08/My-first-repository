940cfa8d86a73b600f1cf94c98c2354246db6119
Jade <jadedickinson@gmail.com>
2015-10-03 22:02:37 +0100

    Code to evaluate and print closest prime numbers to user's input

--- day1ex17.groovy
+++ day1ex17.groovy
@@ -5,3 +5,11 @@ String str = System.console().readLine()
 userInput = Integer.parseInt(str)
 
 if (++)
+
+if ((upper - userInput) = (lower - userInput)) {
+	println "The closest two prime numbers are" + upper + " and " + lower
+} else if ((upper - userInput) < (lower - userInput)) {
+	println "The closest prime number is " + upper
+} else {
+	println "The closest prime number is " + lower
+}

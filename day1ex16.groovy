46e172321ac171b08b42293e3d906e24686761cc
Jade <jadedickinson@gmail.com>
2015-10-03 21:07:57 +0100

    Starting loop
    Could be better to base comparison on individual letters

--- day1ex16.groovy
+++ day1ex16.groovy
@@ -1,4 +1,8 @@
-String input
+String input = ""
 int p1score, p2score = 0
-println "Enter 2 players' rock paper scissors moves, from the options PP, PR, PS, RP, RR, RS, SP, SR or SS."
-input = System.console().readLine()
+println "To enter 2 players' rock paper scissors moves, use the options PP, PR, PS, RP, RR, RS, SP, SR or SS."
+while (p1score-4 != p2score && p2score-4 !=) {
+	println "Enter an option: "
+	input = System.console().readLine()
+	if 
+}

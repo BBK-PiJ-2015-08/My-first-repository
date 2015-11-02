6365907abbf789aa4b9431887cee324698b96d7c
Jade <jadedickinson@gmail.com>
2015-11-02 18:37:28 +0000

    Built basis of program

---
+++ Calculator.java
@@ -0,0 +1,30 @@
+public class Calculator {
+	private int num1;
+	private int num2;
+
+	public int add (int num1, int num2) {
+		int result = num1 + num2;
+		System.out.println(result);
+		return result;
+	}
+
+	public int subtract (int num1, int num2) {
+		int result = num1 - num2;
+		System.out.println(result);
+		return result;
+	}
+
+	public int multiply (int num1, int num2) {
+		int	result = num1 * num2;
+		System.out.println(result);
+		return result;
+	}
+
+	public int divide (int num1, int num2) {
+		double double1 = (double) num1;
+		double double2 = (double) num2;
+		double doubleResult = num1 / num2;
+		System.out.println(doubleResult);
+		return doubleResult;
+	}
+}

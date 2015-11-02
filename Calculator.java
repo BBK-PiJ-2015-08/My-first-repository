3d44a4b19a463069b57f88668a96defd15c12197
Jade <jadedickinson@gmail.com>
2015-11-02 18:43:54 +0000

    Added modulo method which I had missed

--- Calculator.java
+++ Calculator.java
@@ -23,6 +23,12 @@ public class Calculator {
 		double result = num1 / num2;
 		System.out.println(result);
 	}
+
+	void int modulus (int num1, int num2) {
+		double double1 = (double) num1;
+		double double2 = (double) num2;
+		double result = double1 % double2;
+	}
 }
 
 int calculation 

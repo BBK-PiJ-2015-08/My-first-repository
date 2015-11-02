0b891ea37b0b037404b742583ec9c68802672a7a
Jade <jadedickinson@gmail.com>
2015-11-02 19:32:12 +0000

    Separated main method into it's own file

---
+++ Day 6 Ex 1/Main.java
@@ -0,0 +1,32 @@
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+//		System.out.println(calculator.add(1,2));
+		System.out.println("Enter an integer");
+		String numberOne = System.console().readLine();
+		System.out.println("Enter another");
+		String numberTwo = System.console().readLine();
+		System.out.println("Enter +, -, *, / or %");
+		String choice = System.console().readLine();
+		int num1 = Integer.parseInt(numberOne);
+		int num2 = Integer.parseInt(numberTwo);
+		switch(choice) {
+			case "+":
+				System.out.println(calculator.add(num1,num2));
+				break;			
+			case "-":
+				System.out.println(calculator.subtract(num1,num2));
+				break;
+			case "*":
+				System.out.println(calculator.multiply(num1,num2));
+				break;
+			case "/":
+				System.out.println(calculator.divide(num1,num2));
+				break;
+			case "%":
+				System.out.println(calculator.modulus(num1,num2));
+				break;
+			default:
+				System.out.println("Invalid input");
+				break; 
+		}
+	}

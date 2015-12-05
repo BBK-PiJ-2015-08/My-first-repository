c1e872a7816acde7418f454d266a1fb6f0c14125
Jade <jadedickinson@gmail.com>
2015-12-05 19:26:03 +0000

    Updated as per program; minimum to make program work is to rewrite (not override) playGame method in smartPhone class

---
+++ Day10/4/OldPhone.java
@@ -0,0 +1,19 @@
+public class OldPhone implements Phone {
+	private String brand = null;
+
+	public OldPhone() {
+
+	}
+
+	public OldPhone(String brand) {
+		this.brand = brand;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void call(String number) {
+		System.out.println("Calling <" + number + ">...");
+	}
+}

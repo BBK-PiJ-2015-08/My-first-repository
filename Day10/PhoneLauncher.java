2deb39dbb53fc71ff685d91b5ab6544e14e80cf1
Jade <jadedickinson@gmail.com>
2015-12-05 14:59:20 +0000

    Added 11 numbers to test storing only last 10 numbers; works fine.

--- Day10/PhoneLauncher.java
+++ Day10/PhoneLauncher.java
@@ -4,19 +4,24 @@ public class PhoneLauncher {
 		launcher.launch();
 	}
 	public void launch() {
+	// your code creating and using SmartPhone here...
 		SmartPhone iPhone = new SmartPhone();
 		iPhone.ringAlarm("morning");
 		iPhone.findPosition();
 		iPhone.browseWeb("http://www.google.com");
-	// your code creating and using SmartPhone here...
-		/**
-		browseWeb
-		findPosition
-		ringAlarm
-		playGame
-		printLastnumbers
-		call (calls recordNumber)
-
-		*/
+		iPhone.playGame("Snake");
+		iPhone.call("0207 789 1234");
+		iPhone.call("118 118");
+		iPhone.call("0800 888 9911");
+		iPhone.call("999");
+		iPhone.call("0192 890 9000");
+		iPhone.call("0091 982 9098290");
+		iPhone.call("0207 789 4321");
+		iPhone.call("0207 987 1234");
+		iPhone.call("0777 198 9090");
+		iPhone.call("911");
+		// 10th number; next number should shift list up removing first number called.
+		iPhone.call("001 781 787 7777");
+		iPhone.printLastNumbers();
 	}
 }

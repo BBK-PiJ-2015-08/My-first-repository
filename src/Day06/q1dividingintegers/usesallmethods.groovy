4cc573846cb0428f515068f553a89f7219182366
Jade <jadedickinson@googlemail.com>
2016-03-28 11:41:53 +0100

    Groovy program that uses all methods of calculator

---
+++ src/Day06/q1dividingintegers/usesallmethods.groovy
@@ -0,0 +1,8 @@
+package q1dividingintegers
+Calculator calculator = new Calculator();
+
+System.out.println("Addition of 1 and 2: " + calculator.add(1,2));
+System.out.println("Subtract 49 from 106: " + calculator.subtract(106,49));
+System.out.println("Multiply 15 by 4: " + calculator.multiply(15,4));
+System.out.println("Divide 100 by 5: " + calculator.divide(100,5));
+System.out.println("Remainder from dividing 99 by 4: " + calculator.modulus(99,4));
